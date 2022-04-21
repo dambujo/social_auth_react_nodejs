733fbda7a8f336ef09dedabe06d1a69adb0bbce2
Antonio Dambujo <pdambuja@gmail.com>
2022-04-21 17:25:40 +0200

    add navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
+import logo from '../assets/logo192.png';
 const Navbar = () => {
   return (
     <div className="navabr">
       <span className="logo">Dambujo Auth App</span>
       <ul className="list">
         <li className="listItem">
-          <img src="" alt="" className="avatar" />
+          <img src={logo} alt="" className="avatar" />
         </li>
         <li className="listItem">John Doe</li>
         <li className="listItem">Logout</li>
